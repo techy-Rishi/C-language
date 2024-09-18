7e1bdf75105631f9ac4fdcf3050a95a953a16cc5
Rishi Yadav <rahulrishiyadav1994@gmail.com>
2024-09-18 15:29:30 +0530

    Update linear search.c

--- programs/linear search.c
+++ programs/linear search.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+int i,item,a[10]={2,3,4,5,6,7,8,9,34,54},n;
+printf("Enter thr size of array");
+scanf("%d",&n);
+printf("Enter the item to searchg");
+scanf("%d",&item);
+for(i=0; i<=n-1; i++){
+  if(item==a[i]){
+      printf("The location ius %d",i);
